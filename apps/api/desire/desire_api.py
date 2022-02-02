15ece39285d7c6241fe766cf4dbed77186cd3ead
Kamil Boberek <kboberek@gmail.com>
2022-02-02 13:25:46 +0100

    Fatal error fix :)

---
+++ apps/api/desire/desire_api.py
@@ -0,0 +1,19 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+
+class Desire(APIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        data = {}
+
+
+        return Response(data)
+    
+
+    def post(self, request):
+        data = {}
